72f43fef34b09d811c3ce77a298c22f0a949bb38
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-09-06 16:11:09 -0400

    adding javascript animal variable

--- assets/giftastic.js
+++ assets/giftastic.js
@@ -1,1 +1,9 @@
-$()
+$(document).ready(function() {
+
+    var animals = ["rabbit", "star nosed mole","wombat", "blob fish", "pink fairy armadillo","aye-aye","tufted deer","dumbo octopus","naked mole rat","gobi jerboa"]
+
+
+
+
+}
+
