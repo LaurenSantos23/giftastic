5729f0f4ef4a9bb9e690bbd04cc196eb3108d9a2
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-09-06 18:04:29 -0400

    psuedo code for js

--- assets/giftastic.js
+++ assets/giftastic.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
-    var animals = ["rabbit", "star nosed mole","wombat", "blob fish", "pink fairy armadillo","aye-aye","tufted deer","dumbo octopus","naked mole rat","gobi jerboa"]
+//create array of animals, each animal in the array will have a button.  When clicked, the button becomes 10 gifs per animal
+
+    var animals = ["rabbit", "star nosed mole","wombat", "blob fish", "pink fairy armadillo",
+                    "aye-aye","tufted deer","dumbo octopus","naked mole rat","gobi jerboa"]
+
+//make the gifs start animation with a click, and stop animation with a click (on click events)
 
 
+//format the search bar to populate a new set of 10 gifs based on the user's search term
 
 
 })
